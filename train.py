d92807ef79c7b891751667b30630a80a9c0ebb80
cxlab-server <zby_bj@ustc.edu.cn>
2024-04-03 18:35:20 +0800

    take previous frames as input

--- train.py
+++ train.py
@@ -13,17 +13,20 @@ from torch.utils.tensorboard import SummaryWriter
 
 import transformer
 import json
+import matplotlib.pyplot as plt
+import librosa.display
 
 
 feat_path = r'./feat'
 config_path = r'./config'
 
 pts = ['sub-%02d'%i for i in range(1,11)]
-model_name = '4h6l'
+model_name = 'h4l6p3e4000'
 
 with open(os.path.join(config_path,f'{model_name}.json'),'r') as f:
     cfg = json.load(f)['model_config']
 
+prv_frame = cfg['prv_frame']
 batch_size = cfg['batch_size']
 epochs = cfg['epochs']
 lr = cfg['lr']
@@ -54,31 +57,58 @@ def splitData(total_data,total_label):
     train_indices = indices[:split_index]
     test_indices = indices[split_index:]
 
+    train_data_list = []
+    test_data_list = []
+
     # 获取训练集和测试集数据
+    data_padding = np.zeros((1,total_data.shape[1]))
+
+    for idx in train_indices:
+        if idx-prv_frame+1<0:
+            tmp = total_data[0:idx+1]
+            for _ in range(prv_frame-idx-1):
+                tmp=np.insert(tmp,0,data_padding,axis=0)
+            train_data_list.append(tmp)
+        else:
+            train_data_list.append(total_data[idx-prv_frame+1:idx+1])
+    for idx in test_indices:
+        if idx-prv_frame+1<0:
+            tmp = total_data[0:idx+1]
+            for _ in range(prv_frame-idx-1):
+                tmp=np.insert(tmp,0,data_padding,axis=0)
+            test_data_list.append(tmp)
+        else:
+            test_data_list.append(total_data[idx-prv_frame+1:idx+1])
     train_data = total_data[train_indices]
     train_label = total_label[train_indices]
-    test_data = total_data[test_indices]
+    # test_data = total_data[test_indices]
     test_label = total_label[test_indices]
 
     train_data_mean = np.mean(train_data)
     train_data_std = np.std(train_data)
 
+    train_data = np.stack(train_data_list,axis=0)
+    test_data = np.stack(test_data_list,axis=0)
+
     train_data = (train_data-train_data_mean)/train_data_std
     test_data = (test_data-train_data_mean)/train_data_std
     return train_data,train_label,test_data,test_label
 
-for pt in pts[5:6]:
+for pt in pts:
     total_data,total_label = loadData(pt)
     train_data,train_label,test_data,test_label = splitData(total_data,total_label)
 
+    test_mfcc = utils.toMFCC(test_label)
+    test_mel = test_data
 
-    input_dim = test_data.shape[1]
-    output_dim = test_label.shape[1]
+    input_dim = test_data.shape[-1]
+    output_dim = test_label.shape[-1]
 
 
     model = transformer.Model(
         input_dim=input_dim,
         scaled_dim = scaled_dim,
+        prv_dim=prv_frame,
         output_dim=output_dim,
         d_model=d_model,
         nhead=nhead,
@@ -93,12 +123,11 @@ for pt in pts[5:6]:
     test_data = torch.from_numpy(test_data).to(device).type(tensor_type)
     test_label = torch.from_numpy(test_label).to(device).type(tensor_type)
     train_dataset = TensorDataset(train_data,train_label)
-    # test_dataset = TensorDataset(test_data,test_label)
+
     train_dataloader = DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True,pin_memory=True)
 
     writer = SummaryWriter(f'./logs/{pt}/{model_name}')
-    # writer.add_graph(model,test_data)
-    # pbar = tqdm.trange(epochs, desc=f"Epochs")
+
 
     for e in range(epochs):
         model.train()
@@ -115,21 +144,39 @@ for pt in pts[5:6]:
             loss.backward()
             optimizer.step()
 
-        # if (e + 1) % print_interval == 0:
         model.eval()
         test_outputs = model(test_data)
         test_loss = criterion(test_outputs, test_label).detach().cpu().numpy()
         aver_loss = aver_loss/len(train_dataloader)
-        # pbar.set_postfix({'train loss':aver_loss,'test loss':test_loss})
         writer.add_scalar(f'train loss',aver_loss,e)
         writer.add_scalar(f'test loss',test_loss,e)
-        # log_write.write(f'{e}\t\t{aver_loss}\t\t{test_loss}\n')
-        # log_write.flush()
+        model_mfcc = utils.toMFCC(test_outputs.detach().cpu().numpy())
+        eu_dis = 0
+        for i in range(test_mfcc.shape[0]):
+            eu_dis += np.linalg.norm(model_mfcc[i] - test_mfcc[i])
+        mcd = eu_dis/test_mfcc.shape[0]
+        writer.add_scalar(f'test mcd',mcd,e)
 
+    test_outputs = model(test_data).detach().cpu().numpy()
     torch.save({
         'epoch':epochs,
         'model_state_dict':model.state_dict(),
         'optimizer_state_dict':optimizer.state_dict()
         }, f'./res/{pt}/{model_name}.pt')
+    
+    try:
+        origin_melspec_fig = plt.figure()
+        librosa.display.specshow(test_mel,sr=16000,x_axis='time', y_axis='mel')
+        plt.colorbar(format='%+2.0f dB')        
+        plt.title(f'{pt}-origin')
+        writer.add_figure(tag=f"{pt}-origin log Mel spectrogram",figure=origin_melspec_fig)
+
+        model_melspec_fig = plt.figure()
+        librosa.display.specshow(test_outputs,sr=16000,x_axis='time', y_axis='mel')
+        plt.colorbar(format='%+2.0f dB')        
+        plt.title(f'{pt}-model')
+        writer.add_figure(tag=f"{pt}-model log Mel spectrogram",figure=model_melspec_fig)
+    except:
+        pass
     writer.close()
 
